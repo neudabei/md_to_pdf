7779689c4fcbe36c7a620ca68cb6af5f53311a4b
robertsj <rschultzejena@gmail.com>
2024-07-31 13:20:47 +0200

    Display UTF-8 encoded characters
    This adds encoding with utf-8.
    
    I noticed that Spanish accents weren't displayed correctly in the PDF.
    It wasn't due to the markdown renderer, but WickedPDF.

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ require 'pry'
 source_file_content = ARGF.read
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
 
-pdf = WickedPdf.new.pdf_from_string(markdown.render(source_file_content))
+pdf = WickedPdf.new.pdf_from_string(markdown.render(source_file_content), encoding: 'utf-8')
 
 File.open('markdown_as_pdf.pdf', 'wb') do |file|
   file << pdf
